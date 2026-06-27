483bf09284b9cd5ae7cd1ea2e37314d03f378c39
NANCY YU <nancy.yu@ischool.berkeley.edu>
2026-06-26 17:14:31 -0700

    Add Firebase config

--- claude/firebase-config.js
+++ claude/firebase-config.js
@@ -25,7 +25,13 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
 
 const firebaseConfig = {
-FILL IN
+  apiKey: "AIzaSyCOu5MzRhG-CK_VNnlQj4UkZxQ_QW2lADM",
+  authDomain: "puzzle-project-dd8e0.firebaseapp.com",
+  projectId: "puzzle-project-dd8e0",
+  storageBucket: "puzzle-project-dd8e0.firebasestorage.app",
+  messagingSenderId: "898113113919",
+  appId: "1:898113113919:web:9fb2007acfd78d187a8d8a",
+  measurementId: "G-WS510TFSBE"
 };
 
 const app = initializeApp(firebaseConfig);
